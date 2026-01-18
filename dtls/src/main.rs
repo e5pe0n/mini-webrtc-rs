3c2a72dbe721b48b48d0f1c8a3192eeb15be1365
e5pe0n <1izard.synchr0fi5h@gmail.com>
2026-01-18 16:14:07 +0900

    feat: init dtls

--- dtls/src/main.rs
+++ dtls/src/main.rs
@@ -1,3 +1,26 @@
+use rcgen::{CertifiedKey, KeyPair, generate_simple_self_signed};
+use sha2::{
+    Digest, Sha256,
+    digest::generic_array::{GenericArray, typenum::U32},
+};
+
+struct DtlsServer {
+    certified_key: CertifiedKey<KeyPair>,
+    fingerprint: GenericArray<u8, U32>,
+}
+
+impl DtlsServer {
+    pub fn new() -> Self {
+        let certified_key = generate_simple_self_signed(vec![]).unwrap();
+        let fingerprint = Sha256::digest(certified_key.cert.der());
+
+        DtlsServer {
+            certified_key,
+            fingerprint,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    DtlsServer::new();
 }
