cbece07eefeab4c9401cfaa94aef786e358812af
e5pe0n <1izard.synchr0fi5h@gmail.com>
2026-02-25 22:50:23 +0900

    wip: fetch sdp offer

--- browser-app/src/App.tsx
+++ browser-app/src/App.tsx
@@ -1,35 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
+import * as sdpTransform from "sdp-transform";
 
-function App() {
-  const [count, setCount] = useState(0)
+const rtcConfig: RTCConfiguration = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+  ],
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const signalingServerUrl = "http://localhost:3000";
+
+type SdpOffer = {
+  sessionId: string;
+  mediaItems: SdpMedia[];
+};
+
+type SdpMedia = {
+  mediaId: number;
+  type: MediaType;
+  ufrag: string;
+  pwd: string;
+  fingerprintType: FingerprintType;
+  fingerprintHash: string;
+  candidates: SdpMediaCandidate[];
+};
+
+type SdpMediaCandidate = {
+  ip: string;
+  port: number;
+  type: CandidateType;
+  transport: TransportType;
+};
+
+type MediaType = "audio" | "video";
+type CandidateType = "host";
+type TransportType = "udp" | "tcp";
+type FingerprintType = "sha-256";
+
+async function fetchSdpOffer(): Promise<SdpOffer> {
+  const resp = await fetch(`${signalingServerUrl}/offer`);
+  if (!resp.ok) {
+    throw new Error("failed to fetch offer");
+  }
+
+  return await resp.json();
 }
 
-export default App
+function App() {
+  const localVideoRef = useRef<HTMLVideoElement | null>(null);
+  const [peerConnection, setPeerConnection] =
+    useState<RTCPeerConnection | null>(null);
+
+  const [iceCandidates, setIceCandidates] = useState<RTCIceCandidate[]>([]);
+
+  const handleStartMedia = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    if (localVideoRef.current) {
+      localVideoRef.current.srcObject = stream;
+    }
+
+    const pc = new RTCPeerConnection(rtcConfig);
+
+    // fetch offer
+    const offer = await fetchSdpOffer();
+    const offerStr = sdpTransform.write({
+      // TODO
+    });
+    pc.setRemoteDescription({
+      type: "offer",
+      sdp: offerStr,
+    });
+    // create answer
+    // send answer
+
+    for (const track of stream.getTracks()) {
+      pc.addTrack(track, stream);
+    }
+
+    pc.onicecandidate = ({ candidate }) => {
+      if (candidate) {
+        setIceCandidates((value) => [...value, candidate]);
+      }
+    };
+
+    pc.onconnectionstatechange = (ev) => {
+      console.log("connection state changed", ev);
+    };
+
+    pc.oniceconnectionstatechange = (ev) => {
+      console.log("ice connection state changed", ev);
+    };
+
+    pc.onicecandidateerror = (ev) => {
+      console.log("ice candidate error", ev);
+    };
+
+    pc.onicegatheringstatechange = (ev) => {
+      console.log("ice gathering state changed", ev);
+    };
+
+    pc.onsignalingstatechange = (ev) => {
+      console.log("signaling state changed", ev);
+    };
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>Local Video</h3>
+        <video ref={localVideoRef} autoPlay playsInline muted>
+          <track kind="captions" />
+        </video>
+      </div>
+      <div>
+        <button type="button" onClick={handleStartMedia}>
+          start media
+        </button>
+      </div>
+      <div>
+        <label>ice candidates</label>
+        <textarea
+          readOnly
+          rows={4}
+          value={JSON.stringify(iceCandidates, null, 2)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
